4e192e23fad02cd7932568bd0dd49d78314438cf
TimRosenquist <tim.rosenquist@yh.nackademin.se>
2023-03-03 12:56:45 +0100

    Add more UI

--- Assets/Scripts/MySceneManager.cs
+++ Assets/Scripts/MySceneManager.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 namespace Art_VR
 {
     public class MySceneManager : MonoBehaviour
     {
-
+        XRIDefaultInputActions inputActions;
+        InputAction mainMenu;
         public void LoadCreativeScene()
         {
             SceneManager.LoadScene("Creative");
@@ -17,6 +19,20 @@ namespace Art_VR
         {
             SceneManager.LoadScene("Anarchy");
         }
+        public void QuitApplication()
+        {
+            Application.Quit();
+        }
+        private void OnEnable()
+        {
+            inputActions = new XRIDefaultInputActions();
+            mainMenu = inputActions.XRILeftHandInteraction.MainMenu;
+            mainMenu.Enable();
+            mainMenu.performed += MainMenu_performed;
+        }
+        private void MainMenu_performed(InputAction.CallbackContext obj)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
     }
-
 }
